d9a4fd6e23bcc9d9c5ec16762bb505d6b5b8b6c3
ellynhan <wown252@naver.com>
2021-06-04 06:40:51 +0900

    web

--- Web/app.js
+++ Web/app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var boundRouter = require('./routes/boundary');
 
 var app = express();
 
@@ -15,14 +16,14 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/a',indexRouter);
 app.use('/b',indexRouter);
-app.use('/users', usersRouter);
+app.use('/bound', boundRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
