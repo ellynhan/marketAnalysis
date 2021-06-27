7fe73cc1be0faeb7abf0a0fdd87408e1810f5023
ellynhan <wown252@naver.com>
2021-06-27 22:07:31 +0900

    data connect

--- Web/routes/index.js
+++ Web/routes/index.js
@@ -2,19 +2,38 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var router = express.Router();
+var request = require('request');
 
 router.get('/', function(req, res, next) {
   console.log("welcoe to home");
-  var readStream = fs.createReadStream(path.join(__dirname, '../../DATA') + '/answer.txt', 'utf8');
-  var data = ''
+  var readAnswer = fs.createReadStream(path.join(__dirname, '../../DATA') + '/answer.txt', 'utf8');
+  var readFinalData = fs.createReadStream(path.join(__dirname, '../../DATA') + '/final_data.txt', 'utf8');
+  var data = '';
+  var finalData = '';
   var buffers = [];
-  readStream.on('data', function(chunk) {
+  var buffers2 = [];
+  var mapTest = {};
+
+  readAnswer.on('data', function(chunk) {
     data += chunk;
   }).on('end', function() {
-      // console.log(data);
-      buffers = data.split('\n');
-      // console.log(buffers[0]);
-      res.render('index', { scores: buffers });
+    buffers = data.split('\n');
+    readFinalData.on('data', function(chunk2) {
+      finalData += chunk2;
+    }).on('end', function(){
+      buffers2 = finalData.split('\n');
+      buffers2.forEach(element => {
+        var tmp = element.split(':');
+        if(tmp[0]!=undefined && tmp[0]!=''){
+          if(tmp[1]!=undefined){
+            mapTest[tmp[0]] = tmp[1].split(',');
+          }else{
+            mapTest[tmp[0]] = [];
+          }
+        }
+      });
+      res.render('index', { scores: buffers, details: mapTest });
+    })
   });
 });
 
